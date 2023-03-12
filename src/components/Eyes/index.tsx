dc0b66ec166952fb07786d182ab65352d663d03a
Carlos Espinoza <carlosarmikhael@gmail.com>
2023-03-12 03:03:59 -0300

    feat: eyes components

---
+++ src/components/Eyes/index.tsx
@@ -0,0 +1,36 @@
+import styles from "./style.module.scss";
+
+export function Eyes() {
+  return (
+    <>
+      <div className={styles.eyes}>
+        <div className={styles.eye}>
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 84.7 162"
+          >
+            <path
+              className={styles.st0}
+              d="M84.7,81c0,44.7-16.9,80-42.3,80S0,125.7,0,81S19,0,42.3,0S84.7,36.3,84.7,81z"
+            />
+          </svg>
+        </div>
+        <div className={styles.eye}>
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 84.7 162"
+          >
+            <path
+              className={styles.st0}
+              d="M84.7,81c0,44.7-16.9,80-42.3,80S0,125.7,0,81S19,0,42.3,0S84.7,36.3,84.7,81z"
+            />
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+}
